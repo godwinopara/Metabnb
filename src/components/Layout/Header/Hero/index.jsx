2d134273db372d26b53f426d0e46493c82aaf0d0
Godwin Opara <godwinopara62@yahoo.com>
2022-11-12 21:39:10 +0100

    fix: added padding to hero section on mobile screen

--- src/components/Layout/Header/Hero/index.jsx
+++ src/components/Layout/Header/Hero/index.jsx
@@ -3,7 +3,7 @@ import HeroImages from "./HeroImages";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col-reverse lg:px-10 lg:flex-row lg:justify-between max-w-[1240px] mx-auto xl:px-0">
+    <section className="flex flex-col-reverse px-6 lg:px-10 lg:flex-row lg:justify-between max-w-[1240px] mx-auto xl:px-0">
       <HeroDescription />
       <HeroImages />
     </section>
